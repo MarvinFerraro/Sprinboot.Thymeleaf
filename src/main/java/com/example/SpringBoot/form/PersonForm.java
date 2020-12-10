8c7c1559742313d60ae24e22f601e732846737c5
MarvinFerraro <m.ferraro@live.fr>
2020-12-10 11:14:15 +0100

    Init commit, with some modification

---
+++ src/main/java/com/example/SpringBoot/form/PersonForm.java
@@ -0,0 +1,29 @@
+package com.example.SpringBoot.form;
+
+public class PersonForm {
+
+    private int id;
+    private String name;
+    private String type;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
